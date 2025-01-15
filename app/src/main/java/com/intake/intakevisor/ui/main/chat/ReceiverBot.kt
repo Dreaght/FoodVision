598e7d36b167c68414b7e91eee6564cd771e1cba
Dreaght <nikitaefser20092007@gmail.com>
2025-01-15 13:38:51 +0300

    chatbot more polite

--- app/src/main/java/com/intake/intakevisor/ui/main/chat/ReceiverBot.kt
+++ app/src/main/java/com/intake/intakevisor/ui/main/chat/ReceiverBot.kt
@@ -6,23 +6,30 @@ import kotlin.random.Random
 class ReceiverBot {
 
     private val responses = mutableListOf(
-        "I don't know how to help you, fat asshole. Judging by your food, what are you eating, you're a fat ass.",
-        "And? Stop devouring, get involved in sports, stop whining to me here. Nothing's going to help you swine."
+        "Hello! I understand you'd like some help," +
+                " but I'm not sure how to assist you since it seems previous approaches haven't worked." +
+                " I'm here to help in any way I can!",
+        "Engaging in regular physical activity and being mindful of portion sizes could be a good starting point." +
+                " Let me know if you'd like further suggestions!",
+        "I’d recommend reconsidering your current dietary choices and habits." +
+                " Perhaps a fresh perspective on your meals might help. Would you like some examples?",
     )
 
     // Simulate streaming of AI response with delays
     suspend fun getResponseFragments(message: String, onFragmentReceived: (String) -> Unit) {
-        val finalResponse = responses.firstOrNull() ?: "I'm sorry, I don't understand."
+        val finalResponse = responses.firstOrNull() ?: ("Of course!" +
+                " If you have any more questions or need further assistance in the future," +
+                " feel free to reach out. Wishing you the best on your journey to better health and well-being!")
         responses.removeFirstOrNull()
 
         val responseFragments = finalResponse.split(" ")  // Split by words (or sentences) for fragments
 
-        delay(Random.nextLong(500, 1000))
+        delay(Random.nextLong(1000, 1500))
 
         for (fragment in responseFragments) {
             onFragmentReceived(fragment)
 
-            delay(Random.nextLong(50, 500))
+            delay(Random.nextLong(80, 500))
         }
     }
 }
